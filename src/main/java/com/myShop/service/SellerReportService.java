738c828e4bb9ceb267874212a0510cae31d6fc98
AjeetY28 <ajeet.bca28@gmail.com>
2025-03-03 04:56:10 +0530

    created sellerReportService and impl

--- src/main/java/com/myShop/service/SellerReportService.java
+++ src/main/java/com/myShop/service/SellerReportService.java
@@ -1,9 +1,10 @@
 package com.myShop.service;
 
+import com.myShop.entity.Seller;
 import com.myShop.entity.SellerReport;
 
 public interface SellerReportService {
 
-    SellerReport getSellerReport(String sellerId);
+    SellerReport getSellerReport(Seller sellerId);
     SellerReport updateSellerReport(SellerReport sellerReport);
 }
