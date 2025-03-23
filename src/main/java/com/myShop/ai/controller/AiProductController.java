0ea1eb9efd4c43d1daa42e2645c58cd0b6b3a4b4
AjeetY28 <ajeet.bca28@gmail.com>
2025-03-24 02:01:10 +0530

    AiProductController service

--- src/main/java/com/myShop/ai/controller/AiProductController.java
+++ src/main/java/com/myShop/ai/controller/AiProductController.java
@@ -5,6 +5,7 @@ import com.myShop.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AiProductController {
     private final AiProductService productService;
 
 
+    @PostMapping("/chat/demo")
     public ApiResponse generate(@RequestParam(
                         value = "message",
                         defaultValue = "Tell me a joke") String message) throws Exception {
