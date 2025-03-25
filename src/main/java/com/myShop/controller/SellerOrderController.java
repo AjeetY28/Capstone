0e12616c667d7ff270c7b7090adf66d259392b22
AjeetY28 <ajeet.bca28@gmail.com>
2025-03-26 03:05:41 +0530

    change in SellerOrderController

--- src/main/java/com/myShop/controller/SellerOrderController.java
+++ src/main/java/com/myShop/controller/SellerOrderController.java
@@ -3,7 +3,9 @@ package com.myShop.controller;
 import com.myShop.domain.OrderStatus;
 import com.myShop.entity.Order;
 import com.myShop.entity.Seller;
+import com.myShop.exceptions.OrderException;
 import com.myShop.exceptions.SellerException;
+import com.myShop.response.ApiResponse;
 import com.myShop.service.OrderService;
 import com.myShop.service.SellerService;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/seller/orders")
+@RequestMapping("/seller/orders")
 public class SellerOrderController {
 
     private final SellerService sellerService;
@@ -24,9 +26,9 @@ public class SellerOrderController {
     @GetMapping()
     public ResponseEntity<List<Order>> getAllOrderHandler(
             @RequestHeader("Authorization") String jwt
-    ) throws Exception {
+    ) throws SellerException {
         Seller seller=sellerService.getSellerProfile(jwt);
-        List<Order> orders=orderService.sellersOrder(seller.getId());
+        List<Order> orders=orderService.getShopsOrders(seller.getId());
 
         return new ResponseEntity<>(orders, HttpStatus.ACCEPTED);
     }
@@ -36,9 +38,20 @@ public class SellerOrderController {
             @RequestHeader("Authorization") String jwt,
             @PathVariable Long orderId,
             @PathVariable OrderStatus orderStatus)
-        throws Exception{
+        throws OrderException {
 
         Order order=orderService.updateOrderStatus(orderId,orderStatus);
         return new ResponseEntity<>(order,HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{orderId}/delete")
+    public ResponseEntity<ApiResponse>deleteOrderHandler(
+            @PathVariable Long orderId,
+            @RequestHeader("Authorization") String jwt) throws OrderException{
+
+        orderService.deleteOrder(orderId);
+        ApiResponse res=new ApiResponse("Order Deleted Successfully",true);
+
+        return new ResponseEntity<>(res,HttpStatus.ACCEPTED);
     }
 }
