94becaf4860b3607c0942d38f26185ec3d4afcf8
AjeetY28 <ajeet.bca28@gmail.com>
2025-03-25 14:31:30 +0530

    changes in JwtTokenHibernate: select u1_0.id,u1_0.email,u1_0.name,u1_0.password,u1_0.phone,u1_0.role from user u1_0 where u1_0.email=?

--- src/main/java/com/myShop/config/JwtConstant.java
+++ src/main/java/com/myShop/config/JwtConstant.java
@@ -1,8 +1,7 @@
 package com.myShop.config;
 
-public class JWT_CONSTANT {
+public class JwtConstant {
 //    public static final String SECRET_KEY="ahejusaxsadsadsadsadsadu";
     public static final String SECRET_KEY = "mySuperSecureSecretKeyWith32Characters123";
     public static final String JWT_HEADER="Authorization";
-
 }
